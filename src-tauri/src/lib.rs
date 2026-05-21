2694a1eb2883605d54ee7a1c3096ac62abc33935
MUKi <mukispace@gmail.com>
2026-05-22 01:11:54 +0800

    feat: register tauri-plugin-process for relaunch()

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1019,6 +1019,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .setup(|app| {
             app.manage(AppState {
                 repo_path: Mutex::new(None),
